18adaf8d635ce111f8cd1e366f1849ad3591ed3e
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-07-03 15:10:26 -0400

    work on folder

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,24 +1,24 @@
-import { Container, Row, Col } from "../../util/Bootstrap.jsx";
 import { Github } from "react-bootstrap-icons";
+import { Container, Row, Col } from "../../util/Bootstrap.jsx";
+
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer>
-      <Container id="portfolio" className="p-5" >
+      <Container id="footer" className="p-5" >
         <Row>
           <Col>
           <p className="text-center text-secondary">
             <a href={'http://github.com/AnthonyM04/portfolio-web'}
-              target="_blank"
-              rel="noreferrer"
-              className="button-effect">
-            <Github size="40" color="grey" />
-            <br /><small>Code in GitHub</small>
+              className="button-effect"
+              >
+                <Github size={50}/>
             </a>
-            <small> {currentYear} Original AI Art</small></p>
+            <small> {currentYear}</small></p>
           </Col>
+
         </Row>
       </Container>
     </footer>

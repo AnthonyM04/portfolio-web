c5a9ba0053b9b4ec92d5751a2fbbee227f820526
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-07-03 16:36:41 -0400

    more footer work

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -6,21 +6,46 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer>
-      <Container id="footer" className="p-5" >
-        <Row>
-          <Col>
-          <p className="text-center text-secondary">
-            <a href={'http://github.com/AnthonyM04/portfolio-web'}
-              className="button-effect"
-              >
-                <Github size={50}/>
-            </a>
-            <small> {currentYear}</small></p>
-          </Col>
+    // <footer>
+    //   <Container id="footer" className="p-5" >
+    //     <Row>
+    //       <Col>
+    //       <p className="text-center text-secondary">
+    //         <a href={'http://github.com/AnthonyM04/portfolio-web'}
+    //           className="button-effect"
+    //           >
+    //             <Github size={50}/>
+    //         </a>
+    //         <small> {currentYear}</small></p>
+    //       </Col>
 
+    //     </Row>
+    //   </Container>
+    // </footer>
+
+    <footer>
+      <Container id="footer">
+        <Row>
+          {/* <Col className="footer-icons">
+            <Github size={50}/>
+          </Col>
+          <Col className="footer-icons">
+            <Github size={50}/>
+          </Col> */}
+          <Col className="footer-icons">
+            <a href={'http://github.com/AnthonyM04/portfolio-web'}>
+            <Github size={50}/>
+            </a>
+          </Col>
+          {/* <Col className="footer-icons">
+            <Github size={50}/>
+          </Col>
+          <Col className="footer-icons">
+            <Github size={50}/>
+          </Col> */}
         </Row>
       </Container>
-    </footer>
+      <small> {currentYear}</small>
+     </footer>
   );
 }
